333905f4f62f4887f0c47045c34dc36bccbf69f6
THARUSHA <tharushadilmin123@gmail.com>
2025-05-22 20:37:09 +0530

    mobile menu

--- components/shared/header/menu.jsx
+++ components/shared/header/menu.jsx
@@ -1,8 +1,14 @@
-import { EllipsisVertical, Sheet, ShoppingCartIcon, UserIcon } from 'lucide-react';
+import { EllipsisVertical, ShoppingCartIcon, UserIcon } from 'lucide-react';
 import { Button } from '@/components/ui/button';
 import Modetoggle from '@/components/shared/header/mode-toggle';
 import Link from 'next/link';
-import { SheetContent, SheetDescription, SheetTitle, SheetTrigger } from '@/components/ui/sheet';
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
 
 
 
@@ -26,12 +32,17 @@ const Menu = () => {
                 <SheetTrigger className='align-middle'>
                     <EllipsisVertical/>
                 </SheetTrigger>
-                <SheetContent className='flex felx-col items-center'>
-                    <SheetTitle>Menu</SheetTitle>
-                    <Modetoggle/>
-                    <Button asChild variant='ghost'>
+                <SheetContent className='flex felx-col items-start'>
+                    <SheetTitle className='mt-6 ml-3'>Menu</SheetTitle>
+                    <span className='ml-2'> <Modetoggle /> </span>
+                    <Button className='ml-3' asChild variant='ghost'>
                         <Link href='/cart'>
-                            <ShoppingCartIcon/>
+                            <ShoppingCartIcon/> Cart
+                        </Link>
+                    </Button>
+                    <Button className='ml-3' asChild >
+                        <Link href='/sign-in'>
+                            <UserIcon/> Sign-in
                         </Link>
                     </Button>
                     <SheetDescription></SheetDescription>
